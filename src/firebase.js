25b847fc8f7f2f960b0781dc840368375063353a
Asif Mahmud <asiffyy@gmail.com>
2020-12-09 00:00:25 +0600

    code cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCdPJd1uxQsuLXcBdTEavEeT69zCd1y4XM",
-  authDomain: "blue-notes-294ef.firebaseapp.com",
-  databaseURL: "https://blue-notes-294ef.firebaseio.com",
-  projectId: "blue-notes-294ef",
-  storageBucket: "blue-notes-294ef.appspot.com",
-  messagingSenderId: "804429613620",
-  appId: "1:804429613620:web:ef16d7ff93f2e776bf9861"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 const db = firebaseApp.firestore();
